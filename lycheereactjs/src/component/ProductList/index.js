6ba0a20cd9ba326f6423fd56048daca5a6bc602b
ThangChiba <thangchiba@gmail.com>
2022-05-12 09:05:12 -0700

    add layout pc

--- lycheereactjs/src/component/ProductList/index.js
+++ lycheereactjs/src/component/ProductList/index.js
@@ -30,7 +30,7 @@ function ProductList() {
         <Grid container spacing={2}>
           {productsFiltered.map((product) => {
             return (
-              <Grid item xs={12} sm={6} md={4} key={product.id}>
+              <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
                 <ProductThumbnail product={product} />
               </Grid>
             );
