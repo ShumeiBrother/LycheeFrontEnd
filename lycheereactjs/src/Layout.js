90af80baeb81857cbab5b2b102481e64d35532d1
ThangChiba <thangchiba@gmail.com>
2022-05-22 14:35:04 +0900

    Remove LinQ, return Messenger

--- lycheereactjs/src/Layout.js
+++ lycheereactjs/src/Layout.js
@@ -1,12 +1,13 @@
 import { Box } from "@mui/system";
 import Cart from "./component/Cart";
 import Navbar from "./component/Navbar/";
+import MetaMessenger from "../src/component/UIComponent/Common/MetaMessenger";
 function Layout(props) {
   return (
     <Box>
       <Navbar />
       <Cart />
-      {/* <MetaMessenger /> */}
+      <MetaMessenger />
       {props.children}
     </Box>
   );
